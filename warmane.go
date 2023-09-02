bf4ed5591e3aafa0e403dc56414f18196aea8d46
zjiajun <zhujiajun.me@gmail.com>
2023-09-02 12:44:41 +0800

    warmane daily

--- warmane.go
+++ warmane.go
@@ -2,17 +2,61 @@ package main
 
 import (
 	"flag"
+	api2captcha "github.com/2captcha/2captcha-go"
+	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly/v2/extensions"
 	"github.com/golang/glog"
+	"time"
+)
+
+const (
+	warmaneUrl = "https://www.warmane.com"
+	loginUrl   = warmaneUrl + "/account/login"
 )
 
 func init() {
 	flag.Parse()
 }
 func main() {
-
+	glog.Info("warmane collect daily point running")
+	loginData := map[string]string{
+		"return":               "",
+		"userID":               "zhujiajunwow",
+		"userPW":               "jiajunwow23@",
+		"g-recaptcha-response": "",
+	}
 	defer glog.Flush()
-	glog.Info("this is info log")
-	glog.Warning("this is waring log")
-	glog.Error("this is error log")
-	glog.Fatal("this is fatal log")
+
+	c := colly.NewCollector()
+	c.SetRequestTimeout(5 * time.Second)
+	requestInit(c)
+	c.OnResponse(func(response *colly.Response) {
+		glog.Infof("warmane onResponse statusCode %d, body %s",
+			response.StatusCode, string(response.Body))
+	})
+	c.OnError(func(response *colly.Response, err error) {
+		glog.Errorf("warmane login error %s", err.Error())
+	})
+	err := c.Post(loginUrl, loginData)
+	if err != nil {
+		glog.Error("login post err %v", err)
+	}
+	//c.Wait()
+
+}
+
+func requestInit(c *colly.Collector) {
+	//Add Random User agent
+	extensions.RandomUserAgent(c)
+
+	c.OnRequest(func(request *colly.Request) {
+		glog.Infof("warmane onRequest url %s", request.URL)
+		request.Headers.Set("Origin", warmaneUrl)
+		request.Headers.Set("Referer", loginUrl)
+	})
+}
+
+func captchaPass() {
+	client := api2captcha.NewClient("YOUR_API_KEY")
+	client.ApiKey = ""
 }
