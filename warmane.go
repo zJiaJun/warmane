94144d6c73f18a883db02c00fde9bbc9b9014d19
zjiajun <zhujiajun.me@gmail.com>
2023-09-04 16:09:41 +0800

    warmane daily success

--- warmane.go
+++ warmane.go
@@ -19,7 +19,11 @@ const (
 	accountUrl = baseUrl + "/account"
 	loginUrl   = accountUrl + "/login"
 	//see https://cn.2captcha.com/2captcha-api#solving_recaptchav2_new
-	siteKey = "6LfXRRsUAAAAAEApnVwrtQ7aFprn4naEcc05AZUR"
+	siteKey              = "6LfXRRsUAAAAAEApnVwrtQ7aFprn4naEcc05AZUR"
+	csrfTokenSelector    = "meta[name='csrf-token']"
+	coinsSelector        = ".myCoins"
+	pointsSelector       = ".myPoints"
+	loginSuccessBodyText = "{\"redirect\":[\"\\/account\"]}"
 )
 
 func init() {
@@ -34,22 +38,28 @@ func main() {
 	extensions.RandomUserAgent(c)
 
 	csrfToken := ""
-	c.OnHTML("meta[name='csrf-token']", func(element *colly.HTMLElement) {
+	csrfTokenCallback := func(element *colly.HTMLElement) {
 		csrfToken = element.Attr("content")
 		glog.Info("warmane site csrf-token: ", csrfToken)
-	})
-	c.Visit(loginUrl)
+	}
+	c.OnHTML(csrfTokenSelector, csrfTokenCallback)
+	err := c.Visit(loginUrl)
+	if err != nil {
+		glog.Errorf("warmane visit %s error %v", loginUrl, err)
+		return
+	}
+	c.OnHTMLDetach(csrfTokenSelector)
 
 	loginData := map[string]string{
 		"return": "",
 		"userID": "1",
 		"userPW": "2",
 	}
-	code := solveReCaptcha()
+	code := handleCaptcha()
 	loginData["g-recaptcha-response"] = code
 
-	c.OnRequest(func(request *colly.Request) {
-		glog.Infof("warmane onRequest url %s", request.URL)
+	requestCallback := func(request *colly.Request) {
+		glog.Infof("[requestCallback] warmane %s onRequest", request.URL)
 		request.Headers.Set("Origin", baseUrl)
 		request.Headers.Set("Referer", loginUrl)
 		request.Headers.Set("Accept", "application/json, text/javascript, */*; q=0.01")
@@ -58,42 +68,98 @@ func main() {
 		request.Headers.Set("X-Requested-With", "XMLHttpRequest")
 		request.Headers.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7")
 		request.Headers.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+	}
+	c.OnRequest(requestCallback)
 
-	})
-	c.OnResponse(func(response *colly.Response) {
+	/*
+		responseHeadersCallback := func(response *colly.Response) {
+			if loginUrl == response.Request.URL.String() {
+				respCookies := response.Headers.Values("Set-Cookie")
+				glog.Info("[responseHeadersCallback] warmane get cookie: ", respCookies)
+			}
+		}
+		c.OnResponseHeaders(responseHeadersCallback)
+	*/
+
+	responseCallback := func(response *colly.Response) {
 		respBytes, err := decodeBody(response)
 		if err != nil {
-			glog.Error("decode login response body error: ", err)
+			glog.Errorf("[responseCallback] warmane %s decode response body error %v",
+				response.Request.URL, err)
+			return
 		}
-		glog.Infof("warmane login response statusCode %d, body %s", response.StatusCode, string(respBytes))
-	})
+		//需要将解码后的body赋值回去, 否则下面的onHTML无法解析selector
+		response.Body = respBytes
+		glog.Infof("[responseCallback] warmane %s response statusCode %d, body size %d",
+			response.Request.URL, response.StatusCode, len(respBytes))
+		if loginUrl == response.Request.URL.String() && response.Request.Method == "POST" {
+			bodyText := string(response.Body)
+			if bodyText == loginSuccessBodyText {
+				glog.Info("[responseCallback] warmane login success")
+			} else {
+				glog.Error("[responseCallback] warmane login failure: ", bodyText)
+			}
+		}
+		if accountUrl == response.Request.URL.String() && response.Request.Method == "POST" {
+			bodyText := string(response.Body)
+			glog.Info("[responseCallback] warmane collect points body: ", bodyText)
+		}
+	}
+	c.OnResponse(responseCallback)
+
 	loginErr := c.Post(loginUrl, loginData)
 	if loginErr != nil {
 		glog.Error("warmane login error: ", loginErr)
+		return
 	}
 
-	//after login, visit warmane account url
-	c.OnHTML(".wm-ui-hyper-custom-b", func(element *colly.HTMLElement) {
-		attr := element.Attr("data-click")
-		glog.Info("warmane collect points function: ", attr)
+	c.OnHTML(coinsSelector, func(element *colly.HTMLElement) {
+		coins := element.Text
+		glog.Info("warmane account coins: ", coins)
 	})
-	afterLoginErr := c.Visit(accountUrl)
-	if afterLoginErr != nil {
-		glog.Error("after login visit error: ", loginErr)
+	c.OnHTML(pointsSelector, func(element *colly.HTMLElement) {
+		points := element.Text
+		glog.Info("warmane account points: ", points)
+	})
+	accountUrlErr := c.Visit(accountUrl)
+	if accountUrlErr != nil {
+		glog.Error("after login visit error: ", accountUrlErr)
 	}
-
+	collectPointsData := map[string]string{
+		"collectpoints": "true",
+	}
+	collectPointsErr := c.Post(accountUrl, collectPointsData)
+	if collectPointsErr != nil {
+		glog.Error("collect points error: ", collectPointsErr)
+	}
+	c.Visit(accountUrl)
 }
 
-func solveReCaptcha() string {
+func handleCaptcha() string {
 	client := api2captcha.NewClient("")
 	client.DefaultTimeout = 120
 	client.RecaptchaTimeout = 600
 	client.PollingInterval = 30
+
+	b1 := queryBalance(client)
+	code := solveCaptcha(client)
+	b2 := queryBalance(client)
+	glog.Info("2captcha solve captcha cost: ", b2-b1)
+	return code
+}
+
+func queryBalance(client *api2captcha.Client) float64 {
 	balance, err := client.GetBalance()
 	if err != nil {
-		glog.Error("captcha get balance error: ", err)
+		glog.Error("2captcha get balance error: ", err)
 	}
-	glog.Info("captcha account balance: ", balance)
+	glog.Info("2captcha account balance: ", balance)
+	return balance
+}
+
+func solveCaptcha(client *api2captcha.Client) string {
+	glog.Info("2captcha solve captcha begin, waiting......")
+	defer logElapsedTime("2captcha solve captcha finish", time.Now())
 	captcha := api2captcha.ReCaptcha{
 		SiteKey: siteKey,
 		Url:     loginUrl,
@@ -101,10 +167,15 @@ func solveReCaptcha() string {
 	}
 	code, err := client.Solve(captcha.ToRequest())
 	if err != nil {
-		glog.Error("captcha solve error: ", err)
+		glog.Error("2captcha solve error: ", err)
 	}
-	glog.Info("captcha return code: ", code)
+	glog.Info("2captcha return code: ", code)
 	return code
+}
+
+func logElapsedTime(msg string, start time.Time) {
+	duration := time.Since(start)
+	glog.Info(msg+" duration: ", duration)
 }
 
 func decodeBody(response *colly.Response) ([]byte, error) {
