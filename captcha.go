30a071ab43b8458c86e0090f96e3745be82741d0
zjiajun <zhujiajun.me@gmail.com>
2023-09-08 18:11:34 +0800

    opt

---
+++ captcha.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	api2captcha "github.com/2captcha/2captcha-go"
+	"github.com/golang/glog"
+	"time"
+)
+
+type captcha struct {
+	captchaApiKey string
+}
+
+func (c *captcha) HandleCaptcha() (string, error) {
+	client := api2captcha.NewClient(c.captchaApiKey)
+	client.DefaultTimeout = 120
+	client.RecaptchaTimeout = 600
+	client.PollingInterval = 30
+	_, err := queryBalance(client)
+	if err != nil {
+		return "", fmt.Errorf("验证码破解服务查询余额失败, %w", err)
+	}
+	code, err := solveCaptcha(client)
+	if err != nil {
+		return "", fmt.Errorf("验证码破解服务执行失败, %w", err)
+	}
+	return code, nil
+}
+
+func queryBalance(client *api2captcha.Client) (float64, error) {
+	balance, err := client.GetBalance()
+	if err != nil {
+		return 0.0, err
+	}
+	glog.Infof("验证码破解服务可用余额: %f美元", balance)
+	return balance, nil
+}
+
+func solveCaptcha(client *api2captcha.Client) (string, error) {
+	start := time.Now()
+	glog.Info("验证码破解服务开始执行, 需等待1-2分钟")
+	c := api2captcha.ReCaptcha{
+		SiteKey: conf.WarmaneSiteKey,
+		Url:     conf.Url.Login,
+		Action:  "verify",
+	}
+	code, err := client.Solve(c.ToRequest())
+	if err != nil {
+		return "", err
+	}
+	glog.Infof("验证码破解服务执行成功, 耗时 %v", time.Since(start))
+	return code, nil
+}
--- warmane.go
+++ warmane.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
+	"errors"
 	"flag"
-	api2captcha "github.com/2captcha/2captcha-go"
 	"github.com/andybalholm/brotli"
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/extensions"
@@ -13,6 +13,7 @@ import (
 	"gopkg.in/yaml.v3"
 	"io"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -39,7 +40,13 @@ type (
 	}
 )
 
-var conf Config
+var (
+	conf            Config
+	wg              sync.WaitGroup
+	csrfTokenError  = errors.New("查询获取csrfToken错误")
+	confNotFound    = errors.New("配置文件[config.yml]未找到, 请把配置文件放到程序同一目录下")
+	confDecodeError = errors.New("配置文件[config.yml]解析错误, 请检查配置文件")
+)
 
 const (
 	loginSuccessBody         = "{\"redirect\":[\"\\/account\"]}"
@@ -53,24 +60,54 @@ func init() {
 	flag.Parse()
 }
 
-func loadConf() {
+func loadConf() error {
 	file, err := os.ReadFile("conf.yml")
 	if err != nil {
-		glog.Error("can't find conf.yml file")
-		return
+		return confNotFound
 	}
 	err = yaml.Unmarshal(file, &conf)
 	if err != nil {
-		glog.Error("load conf.yml error, ", err)
-		return
+		return confDecodeError
 	}
-	glog.Info("load conf.yml success, ", conf)
+	return nil
 }
 
 func main() {
-	loadConf()
+	glog.Info("开始运行自动签到功能")
 	defer glog.Flush()
-	glog.Info("warmane collect daily point running")
+	err := loadConf()
+	if err != nil {
+		handleError(err)
+		return
+	}
+	count := len(conf.Accounts)
+	glog.Infof("加载配置文件[conf.yml]成功, 需要签到的账号数量是[%d]", count)
+	wg.Add(count)
+	glog.Infof("开始goroutine并发处理")
+	for _, account := range conf.Accounts {
+		go loginAndCollect(account)
+	}
+	wg.Wait()
+}
+
+func handleError(err error) {
+	if err == nil {
+		return
+	}
+	switch err {
+	case confNotFound:
+		glog.Error(err.Error())
+	case confDecodeError:
+		glog.Error(err.Error())
+	case csrfTokenError:
+		glog.Error(err.Error())
+	default:
+		glog.Error(err.Error())
+	}
+}
+
+func loginAndCollect(account Account) {
+	defer wg.Done()
 	c := colly.NewCollector()
 	//允许重复访问URL
 	c.AllowURLRevisit = true
@@ -78,33 +115,29 @@ func main() {
 	//Add Random User agent
 	extensions.RandomUserAgent(c)
 
-	csrfToken := ""
-	csrfTokenCallback := func(element *colly.HTMLElement) {
-		csrfToken = element.Attr("content")
-		glog.Info("warmane site csrf-token: ", csrfToken)
-	}
-	c.OnHTML(conf.Selector.CsrfToken, csrfTokenCallback)
-	err := c.Visit(conf.Url.Login)
+	csrfToken, err := queryCsrfToken(c)
 	if err != nil {
-		glog.Errorf("warmane visit %s error %v", conf.Url.Login, err)
+		handleError(err)
 		return
 	}
-	c.OnHTMLDetach(conf.Selector.CsrfToken)
 
 	loginSuccuss := false
 	loginData := make(map[string]string, 4)
 	loginData["return"] = ""
-	loginData["userID"] = conf.Accounts[0].Username
-	loginData["userPW"] = conf.Accounts[0].Password
-	code := handleCaptcha()
-	if code == "" {
-		glog.Error("2captcha error")
+	loginData["userID"] = account.Username
+	loginData["userPW"] = account.Password
+
+	capt := captcha{
+		captchaApiKey: conf.CaptchaApiKey,
+	}
+	code, err := capt.HandleCaptcha()
+	if err != nil {
+		handleError(err)
 		return
 	}
 	loginData["g-recaptcha-response"] = code
 
 	requestCallback := func(request *colly.Request) {
-		glog.Infof("[requestCallback] warmane %s onRequest", request.URL)
 		request.Headers.Set("Origin", conf.Url.Base)
 		request.Headers.Set("Referer", conf.Url.Login)
 		request.Headers.Set("Accept", "application/json, text/javascript, */*; q=0.01")
@@ -116,129 +149,92 @@ func main() {
 	}
 	c.OnRequest(requestCallback)
 
-	/*
-		responseHeadersCallback := func(response *colly.Response) {
-			if loginUrl == response.Request.URL.String() {
-				respCookies := response.Headers.Values("Set-Cookie")
-				glog.Info("[responseHeadersCallback] warmane get cookie: ", respCookies)
-			}
-		}
-		c.OnResponseHeaders(responseHeadersCallback)
-	*/
-
 	responseCallback := func(response *colly.Response) {
-		respBytes, err := decodeBody(response)
-		if err != nil {
-			glog.Errorf("[responseCallback] warmane %s decode response body error %v",
-				response.Request.URL, err)
+		respBytes, decodeErr := decodeBody(response)
+		if decodeErr != nil {
+			glog.Errorf("解码[%s]返回内容错误, %v", response.Request.URL, err)
 			return
 		}
 		//需要将解码后的body赋值回去, 否则下面的onHTML无法解析selector
 		response.Body = respBytes
-		glog.Infof("[responseCallback] warmane %s response statusCode %d, body size %d",
-			response.Request.URL, response.StatusCode, len(respBytes))
+		glog.Infof("解码[%s]返回内容成功, 状态码:[%d], 大小[%d]", response.Request.URL, response.StatusCode, len(respBytes))
 		if conf.Url.Login == response.Request.URL.String() && response.Request.Method == "POST" {
 			bodyText := string(response.Body)
 			if bodyText == loginSuccessBody {
 				loginSuccuss = true
-				glog.Info("[responseCallback] warmane login success")
+				glog.Infof("账号[%s]登录成功", account.Username)
 			} else {
-				glog.Error("[responseCallback] warmane login failure: ", bodyText)
+				glog.Errorf("账号[%s]登录失败, %s", account.Username, bodyText)
 			}
 		}
 		if conf.Url.Account == response.Request.URL.String() && response.Request.Method == "POST" {
 			bodyText := string(response.Body)
-			glog.Info("[responseCallback] warmane collect points body: ", bodyText)
+			glog.Infof("账号[%s]自动收集签到点, 网站返回内容: %s", account.Username, bodyText)
 		}
 	}
 	c.OnResponse(responseCallback)
 
 	loginErr := c.Post(conf.Url.Login, loginData)
 	if loginErr != nil {
-		glog.Error("warmane login error: ", loginErr)
+		glog.Errorf("账号[%s]登录错误: %v", account.Username, loginErr)
 		return
 	}
 	if loginSuccuss {
+		coins := ""
+		points := ""
 		c.OnHTML(conf.Selector.Coins, func(element *colly.HTMLElement) {
-			coins := element.Text
-			glog.Info("warmane account coins: ", coins)
+			coins = element.Text
 		})
 		c.OnHTML(conf.Selector.Points, func(element *colly.HTMLElement) {
-			points := element.Text
-			glog.Info("warmane account points: ", points)
+			points = element.Text
 		})
-		accountUrlErr := c.Visit(conf.Url.Account)
-		if accountUrlErr != nil {
-			glog.Error("after login visit error: ", accountUrlErr)
+		accErr := c.Visit(conf.Url.Account)
+		if accErr != nil {
+			glog.Errorf("账号[%s]访问账号页面错误: %v", account.Username, accErr)
 			return
 		}
+		glog.Infof("账号[%s]收集签到点[前]的 coins: [%s]", account.Username, coins)
+		glog.Infof("账号[%s]收集签到点[前]的 points: [%s]", account.Username, points)
 		collectPointsData := map[string]string{
 			"collectpoints": "true",
 		}
-		collectPointsErr := c.Post(conf.Url.Account, collectPointsData)
-		if collectPointsErr != nil {
-			glog.Error("collect points error: ", collectPointsErr)
+		accErr = c.Post(conf.Url.Account, collectPointsData)
+		if accErr != nil {
+			glog.Errorf("账号[%s]收集签到点错误: %v", account.Username, accErr)
 			return
 		}
-		err = c.Visit(conf.Url.Account)
-		if err != nil {
+		accErr = c.Visit(conf.Url.Account)
+		if accErr != nil {
+			glog.Errorf("账号[%s]访问账号页面错误: %v", account.Username, accErr)
 			return
 		}
-		err = c.Visit(conf.Url.Logout)
-		if err != nil {
+		glog.Infof("账号[%s]收集签到点[后]的 coins: [%s]", account.Username, coins)
+		glog.Infof("账号[%s]收集签到点[后]的 points: [%s]", account.Username, points)
+		accErr = c.Visit(conf.Url.Logout)
+		if accErr != nil {
+			glog.Errorf("账号[%s]退出错误: %v", account.Username, accErr)
 			return
 		}
+		glog.Infof("账号[%s]退出成功", account.Username)
 	}
 }
 
-func handleCaptcha() string {
-	client := api2captcha.NewClient(conf.CaptchaApiKey)
-	client.DefaultTimeout = 120
-	client.RecaptchaTimeout = 600
-	client.PollingInterval = 30
-
-	code := ""
-	b1 := queryBalance(client)
-	if b1 > 0 {
-		code = solveCaptcha(client)
-		if len(code) > 0 {
-			b2 := queryBalance(client)
-			glog.Info("2captcha solve captcha cost: ", b2-b1)
-		}
+func queryCsrfToken(c *colly.Collector) (string, error) {
+	csrfToken := ""
+	csrfTokenCallback := func(element *colly.HTMLElement) {
+		csrfToken = element.Attr("content")
 	}
-	return code
-}
-
-func queryBalance(client *api2captcha.Client) float64 {
-	balance, err := client.GetBalance()
+	c.OnHTML(conf.Selector.CsrfToken, csrfTokenCallback)
+	err := c.Visit(conf.Url.Login)
 	if err != nil {
-		glog.Error("2captcha get balance error, ", err)
-		return 0
+		return "", csrfTokenError
 	}
-	glog.Info("2captcha account balance: ", balance)
-	return balance
-}
-
-func solveCaptcha(client *api2captcha.Client) string {
-	glog.Info("2captcha solve captcha begin, waiting......")
-	defer logElapsedTime("2captcha solve captcha finish", time.Now())
-	captcha := api2captcha.ReCaptcha{
-		SiteKey: conf.WarmaneSiteKey,
-		Url:     conf.Url.Login,
-		Action:  "verify",
+	c.OnHTMLDetach(conf.Selector.CsrfToken)
+	if csrfToken == "" {
+		return "", csrfTokenError
 	}
-	code, err := client.Solve(captcha.ToRequest())
-	if err != nil {
-		glog.Error("2captcha solve error, ", err)
-		return ""
-	}
-	glog.Info("2captcha return code: ", code)
-	return code
-}
-
-func logElapsedTime(msg string, start time.Time) {
-	duration := time.Since(start)
-	glog.Info(msg+" duration: ", duration)
+	glog.Infof("查询获取warmane网站的csrfToken成功: %s", csrfToken)
+	return csrfToken, nil
 }
 
 func decodeBody(response *colly.Response) ([]byte, error) {
